3a8ccdb1c6e698beaf5b5bf402b0d15c17c81ed4
Diogo Varaschin <diogovaraschin@outlook.com>
2025-02-18 10:39:03 -0300

    aula 01 - contruindo a base do projeto | concluida

--- events/src/main/java/br/com/nlw/events/repo/EventRepo.java
+++ events/src/main/java/br/com/nlw/events/repo/EventRepo.java
@@ -1,5 +1,9 @@
 package br.com.nlw.events.repo;
 
-public interface EventRepo {
+import org.springframework.data.repository.CrudRepository;
 
+import br.com.nlw.events.model.Event;
+
+public interface EventRepo extends CrudRepository <Event, Integer>{
+	public Event findByPrettyName(String prettyName);
 }
