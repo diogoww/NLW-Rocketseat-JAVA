432a448209e435612623786dc89838b10ae5f7dc
Diogo Varaschin <diogovaraschin@outlook.com>
2025-02-19 10:26:03 -0300

    create the SubscriptionRankingByUser.java and SubscriptionResponse.java

--- events/src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ events/src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -2,6 +2,7 @@ package br.com.nlw.events.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.nlw.events.dto.ErrorMessage;
 import br.com.nlw.events.dto.SubscriptionConflictException;
+import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.UserIndicadorNotFoundException;
-import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
 import br.com.nlw.events.service.SubscriptionService;
 
@@ -24,7 +25,7 @@ public class SubscriptionController {
 	@PostMapping({"/subscription/{prettyName}", "/subscription/{prettyName}/{userId}"})
 	public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber, @PathVariable (required = false) Integer userId){
 		try {
-		Subscription res = service.createNewSubscription(prettyName, subscriber, userId);
+			SubscriptionResponse res = service.createNewSubscription(prettyName, subscriber, userId);
 		if (res != null) {
 			return ResponseEntity.ok(res);
 		}
@@ -43,4 +44,15 @@ public class SubscriptionController {
 		
 		return ResponseEntity.badRequest().build();
 	}
+	
+	@GetMapping("/subscription/{prettyName}/ranking")
+	public ResponseEntity<?> generateRankingByEvent(@PathVariable String prettyName) {
+		try {
+			return ResponseEntity.ok(service.getCompleteRanking(prettyName).subList(0, 3));
+		} catch (EventNotFoundException e) {
+			return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
+		}
+	}
+	
+	
 }
--- events/src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ events/src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,9 +1,14 @@
 package br.com.nlw.events.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.nlw.events.dto.SubscriptionConflictException;
+import br.com.nlw.events.dto.SubscriptionRankingByUser;
+import br.com.nlw.events.dto.SubscriptionRankingItem;
+import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.UserIndicadorNotFoundException;
 import br.com.nlw.events.model.Event;
@@ -25,7 +30,7 @@ public class SubscriptionService {
 	@Autowired
 	private SubscriptionRepo subRepo;
 	
-	public Subscription createNewSubscription(String eventName, User user, Integer userId) {
+	public SubscriptionResponse createNewSubscription(String eventName, User user, Integer userId) {
 		
 		// recuperar o evento pelo nome
 		Event evt = evtRepo.findByPrettyName(eventName);
@@ -52,12 +57,23 @@ public class SubscriptionService {
 		
 		Subscription tmpSub = subRepo.findByEventAndSubscriber(evt, userRec);
 		if (tmpSub != null) { //caso alternativo 3
-			throw new SubscriptionConflictException("Ja existe inscrição para o usuário "+userRec.getName());
+			throw new SubscriptionConflictException("Ja existe inscrição para o usuário "+userRec.getName()+" no evento "+evt.getTitle());
 		}
 		
 		Subscription res = subRepo.save(subs);
 		
-		return new Subscription(res.getSubscriptionNumber(), "http://codecraft.com/subscription/"+res.getEvent().getPrettyName()+"/"+res.getSubscriber().getId());
+		return new SubscriptionResponse(res.getSubscriptionNumber(), "http://codecraft.com/subscription/"+res.getEvent().getPrettyName()+"/"+res.getSubscriber().getId());
 	}
 	
+	public List<SubscriptionRankingItem> getCompleteRanking(String prettyName){
+		Event evt = evtRepo.findByPrettyName(prettyName);
+		if (evt == null) {
+			throw new EventNotFoundException("ranking do evento "+prettyName+" não existe");
+		}
+		return subRepo.generateRanking(evt.getEventId());
+	}
+	
+	public SubscriptionRankingByUser getRankingByUser(String prettyName, Integer userId) {
+		return null;
+	}
 }
